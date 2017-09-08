b54397ca4a32f1c89a062bf28861db34f67ed264
patric <patric@patriccampbell.com>
2017-09-08 09:22:53 -0400

    Add Kilobyte to projects list

--- js/script.js
+++ js/script.js
@@ -7,11 +7,18 @@ const projects = [
       description: 'Glitch is a real time chatting web application built using PostgresSQL as its database, Ruby on Rails as the backend, and React with Redux on the frontend. Glitch is inspired by Slack and allows users to chat in public channels as well as two person and multi-person direct messages.'
   },
   {
+    title: 'Kilobyte',
+    projectURL: 'https://patriccampbell.github.io/Kilobyte/',
+    image: 'https://res.cloudinary.com/drhyh4h0r/image/upload/v1504876285/kilobyte_gkafea.png',
+    githubURL: 'https://github.com/patriccampbell/kilobyte',
+    description: 'Kilobyte is a browser game based off of 1024. It is written in vanilla JavaScript and uses the Animate.css CSS library for its animations.'
+  },
+  {
     title: 'YelpCamp',
     projectURL: 'https://limitless-ravine-76629.herokuapp.com/',
     image: 'https://res.cloudinary.com/drhyh4h0r/image/upload/v1492695251/yelpcamp_enqrbw.png',
     githubURL: 'https://github.com/PatricCampbell/yelpcamp',
-    description: 'YelpCamp project made for the Udemy Complete Web Develper course. Backend is written in NodeJS with ExpressJS and a MongoDB database. This app uses RESTful principles for its API. Frontend uses pug templates.'
+    description: 'YelpCamp is a full stack web application inspired by Yelp. The backend is written in NodeJS with ExpressJS and a MongoDB database. It utilizies RESTful principles for its API and the frontend uses pug templates.'
   },
   {
     title: 'User Registration Form',
